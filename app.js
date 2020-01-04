4ffd6fd4759946dba47a875c64e083b7591cb535
David Maghradze <davidmaghradze07@gmail.com>
2020-01-04 04:30:55 +0400

    new commit

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ app.use("/api/link", links);
 app.use("/t", redirect);
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(path.join(__dirname, "client", "build")));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname), "client", "build", "index.html");
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
-const PORT = config.get("port") || 5000;
+const PORT = process.env.PORT || 5000;
 
 async function start() {
   try {
